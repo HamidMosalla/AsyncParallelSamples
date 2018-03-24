8d470c2471f50117b11f1bc6f1003a9fb33479ff
Hamid Mosalla <Xellarix@gmail.com>
2018-03-24 09:48:25 +0430

    add example for releasing the thread back to the caller

--- AsyncParallelSamples/Program.cs
+++ AsyncParallelSamples/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ConsoleSandbox;
 
 namespace AsyncParallelSamples
 {
@@ -10,7 +11,7 @@ namespace AsyncParallelSamples
     {
         static void Main(string[] args)
         {
-
+            AwaitReleaseThreadToCaller.Print();
         }
     }
 }
