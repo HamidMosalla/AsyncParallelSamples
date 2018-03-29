b99ae60a6ef0ca1ad34a2aef8367f0a4411eea2a
Hamid Mosalla <Xellarix@gmail.com>
2018-03-29 11:35:40 +0430

    add class to show how async api changed

---
+++ AsyncParallelSamples/Part2/AsyncThenAndNow.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncParallelSamples.Part2
+{
+    public class AsyncThenAndNow
+    {
+        private Task<decimal> CalculateMeaningOfLifeAsync()
+        {
+            return Task.FromResult(0.0m);
+        }
+
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            Task<decimal> calculation = CalculateMeaningOfLifeAsync();
+
+            calculation.ContinueWith(calculationTask =>
+            {
+                if (!(calculationTask.Exception is AggregateException errors))
+                {
+                    Console.WriteLine(calculation.Result);
+                }
+                else
+                {
+                    Exception actualException = errors.InnerExceptions.First();
+                    Console.WriteLine(actualException.Message);
+                }
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+    }
+}
