b7327989e07163a28ec29ee645717ede8cc825d8
Hamid Mosalla <Xellarix@gmail.com>
2018-04-07 21:32:12 +0430

    add Parallel.Invoke sample

---
+++ AsyncParallelSamples/Misc/ParallelMethodInvocation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncParallelSamples.Misc
+{
+    public class ParallelMethodInvocation
+    {
+        static void ProcessArray(double[] array)
+        {
+            Parallel.Invoke(
+                () => ProcessPartialArray(array, 0, array.Length / 2),
+                () => ProcessPartialArray(array, array.Length / 2, array.Length)
+            );
+        }
+
+        static void ProcessPartialArray(double[] array, int begin, int end)
+        {
+            // CPU-intensive processing...
+        }
+
+        static void DoAction20Times(Action action)
+        {
+            Action[] actions = Enumerable.Repeat(action, 20).ToArray();
+            Parallel.Invoke(actions);
+        }
+    }
+}
