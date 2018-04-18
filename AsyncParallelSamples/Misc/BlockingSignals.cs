38a9374a4c7265b7087562296495fc5e233676eb
Hamid Mosalla <Xellarix@gmail.com>
2018-04-18 14:00:03 +0430

    add blocking signal example

---
+++ AsyncParallelSamples/Misc/BlockingSignals.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncParallelSamples.Misc
+{
+    public class BlockingSignals
+    {
+        private readonly ManualResetEventSlim _initialized =  new ManualResetEventSlim();
+        private int _value;
+
+        public int WaitForInitialization()
+        {
+            _initialized.Wait();
+            return _value;
+        }
+
+        public void InitializeFromAnotherThread()
+        {
+            _value = 13;
+            _initialized.Set();
+        }
+    }
+}
--- AsyncParallelSamples/Program.cs
+++ AsyncParallelSamples/Program.cs
@@ -3,15 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AsyncParallelSamples.Misc;
 using AsyncParallelSamples.Part2;
 
 namespace AsyncParallelSamples
 {
     class Program
     {
+        static int value;
+        static BlockingSignals blockThingy = new BlockingSignals();
+
         static void Main(string[] args)
         {
             AwaitReleaseThreadToCaller.Print();
+
+            Task.Run(() => MainAsync());
+
+            var theValue = value;
+
+            blockThingy.InitializeFromAnotherThread();
+
+            theValue = value;
+        }
+
+        private static async void MainAsync()
+        {
+            await Task.Run(() =>
+            {
+                value = blockThingy.WaitForInitialization();
+            });
         }
     }
 }
