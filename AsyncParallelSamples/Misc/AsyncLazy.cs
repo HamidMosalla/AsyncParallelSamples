e3e141fe742f35819b996b1efa4c5db0323b3e05
Hamid Mosalla <Xellarix@gmail.com>
2018-04-20 12:40:04 +0430

    add comment for lazyAsync

--- AsyncParallelSamples/Misc/AsyncLazy.cs
+++ AsyncParallelSamples/Misc/AsyncLazy.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 
 namespace AsyncParallelSamples.Misc
 {
+    //More info: https://blogs.msdn.microsoft.com/pfxteam/2011/01/15/asynclazyt/
     public class AsyncLazy<T> : Lazy<Task<T>>
     {
         public AsyncLazy(Func<T> valueFactory) : base(() => Task.Factory.StartNew(valueFactory)) { }
