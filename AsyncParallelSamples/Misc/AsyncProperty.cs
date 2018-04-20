9281cc597666f32def8fed6a08b0cc2977e43e9b
Hamid Mosalla <Xellarix@gmail.com>
2018-04-20 12:17:53 +0430

    add async lazy

---
+++ AsyncParallelSamples/Misc/AsyncProperty.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncParallelSamples.Misc
+{
+    public class AsyncProperty
+    {
+        // As a cached value.
+        public AsyncLazy<int> Data
+        {
+            get { return _data; }
+        }
+
+        private readonly AsyncLazy<int> _data = new AsyncLazy<int>(async () =>
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            return 13;
+        });
+    }
+}
