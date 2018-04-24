4696ca25e2a84034d1fa94a5488f7d64fef41804
Hamid Mosalla <Xellarix@gmail.com>
2018-04-24 19:02:15 +0430

    add more example for immutable collection

--- AsyncParallelSamples/Collections/ImmutableCollections.cs
+++ AsyncParallelSamples/Collections/ImmutableCollections.cs
@@ -55,5 +55,69 @@ namespace AsyncParallelSamples.Collections
             // Displays "13"
             Trace.WriteLine(nextItem);
         }
+
+        public static void UseImmutableList()
+        {
+            var list = ImmutableList<int>.Empty;
+            list = list.Insert(0, 13);
+            list = list.Insert(0, 7);
+            // Displays "7" followed by "13".
+            foreach (var item in list)
+                Trace.WriteLine(item);
+            list = list.RemoveAt(1);
+        }
+
+        public static void UseImmutableSet()
+        {
+            var hashSet = ImmutableHashSet<int>.Empty;
+            hashSet = hashSet.Add(13);
+            hashSet = hashSet.Add(7);
+            // Displays "7" and "13" in an unpredictable order.
+            foreach (var item in hashSet)
+                Trace.WriteLine(item);
+            hashSet = hashSet.Remove(7);
+
+
+            var sortedSet = ImmutableSortedSet<int>.Empty;
+            sortedSet = sortedSet.Add(13);
+            sortedSet = sortedSet.Add(7);
+            // Displays "7" followed by "13".
+            foreach (var item in hashSet)
+                Trace.WriteLine(item);
+            var smallestItem = sortedSet[0];
+            // smallestItem == 7
+            sortedSet = sortedSet.Remove(7);
+        }
+
+        public static void UseImmutableDictionary()
+        {
+            var dictionary = ImmutableDictionary<int, string>.Empty;
+
+            dictionary = dictionary.Add(10, "Ten");
+            dictionary = dictionary.Add(21, "Twenty-One");
+            dictionary = dictionary.SetItem(10, "Diez");
+
+            // Displays "10Diez" and "21Twenty-One" in an unpredictable order.
+            foreach (var item in dictionary)
+                Trace.WriteLine(item.Key + item.Value);
+
+            var ten = dictionary[10];
+            // ten == "Diez"
+            dictionary = dictionary.Remove(21);
+
+
+            var sortedDictionary = ImmutableSortedDictionary<int, string>.Empty;
+
+            sortedDictionary = sortedDictionary.Add(10, "Ten");
+            sortedDictionary = sortedDictionary.Add(21, "Twenty-One");
+            sortedDictionary = sortedDictionary.SetItem(10, "Diez");
+
+            // Displays "10Diez" followed by "21Twenty-One".
+            foreach (var item in sortedDictionary)
+                Trace.WriteLine(item.Key + item.Value);
+            var ten2 = sortedDictionary[10];
+            // ten == "Diez"
+            sortedDictionary = sortedDictionary.Remove(21);
+        }
     }
 }
