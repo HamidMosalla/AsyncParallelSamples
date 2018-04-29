68f9ba20572c9d5789669c83ab0224ce0767a76f
Hamid Mosalla <Xellarix@gmail.com>
2018-04-29 17:05:45 +0430

    add ConcurrentDictionary sample

--- AsyncParallelSamples/Collections/ConcurrentCollections.cs
+++ AsyncParallelSamples/Collections/ConcurrentCollections.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,21 @@ namespace AsyncParallelSamples.Collections
 {
     public static class ConcurrentCollections
     {
+
+        public static void UseConcurrentDictionary()
+        {
+            var dictionary = new ConcurrentDictionary<int, string>();
+
+            var newValue = dictionary.AddOrUpdate(0, key => "Zero", (key, oldValue) => "Zero");
+
+            // Using the same "dictionary" as above.
+            bool keyExists = dictionary.TryGetValue(0, out var currentValue);
+
+            // Using the same "dictionary" as above.
+            bool keyExisted = dictionary.TryRemove(0, out var removedValue);
+
+            dictionary[0] = "Zero";
+        }
 
     }
 }
